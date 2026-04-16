3421b2468baad11e60fd0708f9759cb96fb16307
Shanop099 <guptaishan0660@gmail.com>
2026-04-16 21:27:27 +0530

    Fix PORT assignment in index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,7 +12,7 @@ import publicRouter from './routes/public';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.3001 || PORT;
 
 /**
  * FRONTEND URLs (for CORS)
